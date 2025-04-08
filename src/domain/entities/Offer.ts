aeb8cf4bc75d0b031844edd3ffabb1878772c3f2
Lucas Affre <laffre@frba.utn.edu.ar>
2025-04-07 23:16:32 -0300

    Entidades ya listas segun el diagrama

--- src/domain/entities/Offer.ts
+++ src/domain/entities/Offer.ts
@@ -1,16 +1,31 @@
+import { Card } from "./Card";
+import { StatusOffer } from "./StatusOffer";
+
 export interface OfferProps {
     id?: string;
+    cardOffers: Card[];
+    statusOffer: StatusOffer;
+    moneyOffer: number;
+    closedAt?: Date;
     createdAt?: Date;
     updatedAt?: Date;
 }
 
 export class Offer {
     private readonly id: string;
+    private cardOffers: Card[];
+    private statusOffer: StatusOffer;
+    private moneyOffer: number;
+    private closedAt?: Date;
     private readonly createdAt: Date;
     private updatedAt: Date;
 
     constructor(props: OfferProps) {
         this.id = props.id || this.generateId();
+        this.cardOffers = props.cardOffers;
+        this.statusOffer = props.statusOffer;
+        this.moneyOffer = props.moneyOffer;
+        this.closedAt = props.closedAt;
         this.createdAt = props.createdAt || new Date();
         this.updatedAt = props.updatedAt || new Date();
     }
