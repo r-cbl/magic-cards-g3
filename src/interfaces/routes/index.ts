14ba862ad906bc8eb93bdbce3f3155085098fdd2
fmarsico8 <102746644+fmarsico8@users.noreply.github.com>
2025-04-10 13:16:22 -0300

    feat: add documentation for users endpoints

--- src/interfaces/routes/index.ts
+++ src/interfaces/routes/index.ts
@@ -2,13 +2,15 @@ import { Router, Request, Response } from 'express';
 import userRoutes from './user.routes';
 import authRoutes from './auth.routes';
 import { healthCheck } from '../../infrastructure/http/health';
+import { swaggerSpec } from '../../docs/swaggerConfig';
+import swaggerUi from 'swagger-ui-express';
 
 const router = Router();
 
 // API routes
 router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
-
+router.use('/docs',swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 // Health check routes
 router.get('/health', (req: Request, res: Response) => healthCheck(req, res));
 router.get('/health-check', (req: Request, res: Response) => healthCheck(req, res));
