6e35d575204f5c4b05616b28735432ca5f1176f2
Lucas Affre <laffre@frba.utn.edu.ar>
2025-04-10 18:39:56 -0300

    Fix offer changes

--- src/domain/entities/Offer.ts
+++ src/domain/entities/Offer.ts
@@ -63,7 +63,38 @@ export class Offer {
     }
     
     public isMyOffer(offerOwner: User): boolean {
-        return this.offerOwner.getId() === offerOwner.getId(); //TODO: despues hay que cambiarlo para que use el repositorio. 
-    }           
+        return this.offerOwner.getId() === offerOwner.getId();
+    }
 
+    public getId(): string {
+        return this.id;
+    }
+
+    public getOfferOwner(): User {
+        return this.offerOwner;
+    }
+
+    public getCardOffers(): Card[] | undefined {
+        return this.cardOffers;
+    }
+
+    public getStatusOffer(): StatusOffer {
+        return this.statusOffer;
+    }
+
+    public getMoneyOffer(): number | undefined {
+        return this.moneyOffer;
+    }
+
+    public getClosedAt(): Date | undefined {
+        return this.closedAt;
+    }
+
+    public getCreatedAt(): Date {
+        return this.createdAt;
+    }
+
+    public getUpdatedAt(): Date {
+        return this.updatedAt;
+    }
 }
