d76f47bee71903761ec570f454bb400cc271ce91
Lucas Affre <laffre@frba.utn.edu.ar>
2025-04-25 00:09:40 -0300

    Cambios para que corra la pagina principal

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import Publications from './pages/publications.jsx';
+import ViewPublications from './components/viewPublications/viewPublications.jsx';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/publications" element={<Publications />} />
+        <Route path="/publications" element={<ViewPublications />} />
       </Routes>
     </div>
   );
